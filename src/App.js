e124648bc4a851cbee6bc94b48975030b4394441
Jingjo_D <kitty.username@hotmail.com>
2019-04-13 12:18:00 +0700

    TESTANS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React, { Component } from 'react'
 import './App.css'
+import Modal from 'react-modal'
+//import axios from 'axios'
+//TT
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    height:500,
+    width:1000
+
+  }
+};
+
 
 const COLORS = {
   Psychic: "#f8a5c2",
@@ -16,9 +34,127 @@ const COLORS = {
 }
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      cards: [],
+      data: [],
+      myList : [],
+      modalIsOpen: false
+
+    }
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.listData = this.listData.bind(this)
+    this.addNewCard = this.addNewCard.bind(this)
+    this.removedCard = this.removedCard.bind(this)
+  }
+
+  async componentDidMount(){
+    const response = await fetch(`http://localhost:3030/api/cards`);
+    const json = await response.json();
+    this.setState({ cards: json.cards });
+    this.listData(json.cards)
+    
+  }
+
+  listData(data){
+    //console.log(data)
+    this.setState({
+      data: data
+    })
+  }
+  
+  openModal() {
+    this.setState({modalIsOpen: true});
+    //console.log(this.state.cards)
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  addNewCard(Data){
+    //console.log(Data/)
+    this.setState({
+      myList: this.state.myList.concat([Data])
+    })
+    
+  }
+  removedCard(key){
+    let index = this.state.myList.indexOf(key)
+    console.log(index)
+    this.setState({
+      myList: this.state.myList.splice(index)
+    })
+    
+  }
+
+
   render() {
+    var {cards, myList,data} = this.state
     return (
       <div className="App">
+        <div className="Container">
+          <div className="Box">
+            My PokeDek
+          </div>
+          <div className="Box2">
+          {myList.map((item ,key)=>
+              <div className="cards2">
+              <img src={item.imageUrl} style={{height:300}}/>
+              <div>
+                name : {item.name}<br></br>
+                <lable className="addcard" index={key} onClick={()=>{this.removedCard(item)}}>Remove Card</lable>
+              </div>
+          </div>
+              
+              )}
+          </div>
+          <div className="Box3">
+                  <div className="D1">
+                    <button onClick={this.openModal}>Click</button>
+                  </div>
+          </div>
+        </div>
+
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <input type="text" placeholder="Find Pokemon" />
+          {//data.map((item)=> <li>{item.name}</li>)}
+              data.map((item)=>
+                  <div className="cards1">
+                      <img src={item.imageUrl} style={{height:300}}/>
+                      <div>
+                        name : {item.name}<br></br>
+                        <lable className="addcard" onClick={()=>{this.addNewCard(item)}}>Add Card</lable>
+                      </div>
+                  </div>
+                
+              )
+          }
+         
+          
+
+          <button onClick={this.closeModal}>close</button>
+         {}
+          <form>
+          
+          </form>
+        </Modal>
+      
       </div>
     )
   }
--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ const { cards } = require('./../mock/cards.json')
 
 app.use(cors())
 
+
 app.get('/api/cards', (req, res) => {
-  const { name, type, limit = 20 } = req.query
+  let { name, type, limit = 20 } = req.query
 
   if (_.every([name, type], item => item === undefined)) {
     return res.json({ cards: cards.slice(0, limit) })
@@ -25,5 +26,44 @@ app.get('/api/cards', (req, res) => {
   })
 
 })
+
+/*
+app.get('/api/cards', (req, res) => {
+  
+    let { name, type, limit = 20 } = req.query
+ 
+  if (_.every([name, type], item => item === undefined)) {
+    return res.json({ cards: cards.slice(0, limit) }) }
+    /*
+    let newData = cards.map((item)=>{
+      let newHp = (parseInt(item.hp) >= 100) ? 100 : item.hp
+      if(!newHp){
+        newHp = 0
+      }
+      return {
+        cards:{
+          id: item.id,
+          hp: newHp
+        }
+      }
+
+    })*/
+
+  
+ 
+/*
+  res.json({
+    cards: _.filter(cards, card => {
+      const name = _.toUpper(_.get(req, 'query.name', ''))
+      const type = _.toUpper(_.get(req, 'query.type', ''))
+      const checkName = _.includes(_.toUpper(card.name), name)
+      const checkType = _.includes(_.toUpper(card.type), type)
+      return checkName && checkType
+    })
+  })
+
+})*/
+
+
 
 app.listen(3030, () => console.log('app start @ port 3030'))
